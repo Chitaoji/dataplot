3b24e49b82652215c7d23c96cb87fa21a4b6af66
Chitaoji <2360742040@qq.com>
2023-12-05 23:23:42 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-"""Setup the package."""
+"""
+Setup the package.
+
+To use this file, you must:
+
+```sh
+$ pip install twine
+$ pip install wheel
+```
+"""
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
 import os
 import re
 import sys
