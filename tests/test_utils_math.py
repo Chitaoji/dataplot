5b8a18f7e1f08ffa5a8d9010581a8381ed5fa82d
Chitaoji <148310134+Chitaoji@users.noreply.github.com>
2026-04-24 16:03:48 +0800

    test: migrate tests to unittest and expand coverage

--- tests/test_utils_math.py
+++ tests/test_utils_math.py
@@ -1,31 +1,36 @@
+import unittest
+
 import numpy as np
 
 from dataplot.utils.math import get_prob, get_quantile, linear_regression_1d
 
 
-def test_linear_regression_1d_ignores_nan_and_inf_values():
-    x = np.array([1.0, 2.0, 3.0, np.nan, np.inf, -np.inf])
-    y = np.array([3.0, 5.0, 7.0, 11.0, np.nan, np.inf])
+class TestMathUtils(unittest.TestCase):
+    def test_linear_regression_1d_ignores_nan_and_inf_values(self):
+        x = np.array([1.0, 2.0, 3.0, np.nan, np.inf, -np.inf])
+        y = np.array([3.0, 5.0, 7.0, 11.0, np.nan, np.inf])
 
-    a, b = linear_regression_1d(y=y, x=x)
+        a, b = linear_regression_1d(y=y, x=x)
 
-    assert np.isclose(a, 1.0)
-    assert np.isclose(b, 2.0)
+        self.assertTrue(np.isclose(a, 1.0))
+        self.assertTrue(np.isclose(b, 2.0))
+
+    def test_get_quantile_handles_infinite_values_as_nan(self):
+        data = np.array([1.0, 2.0, 3.0, np.inf, -np.inf])
+        p = np.array([0.0, 0.5, 1.0])
+
+        quantiles = get_quantile(data, p)
+
+        self.assertTrue(np.allclose(quantiles, np.array([1.0, 2.0, 3.0])))
+
+    def test_get_prob_uses_only_finite_value_count(self):
+        data = np.array([1.0, 2.0, 3.0, np.inf, -np.inf])
+        q = np.array([0.5, 2.0, 10.0])
+
+        probs = get_prob(data, q)
+
+        self.assertTrue(np.allclose(probs, np.array([0.0, 2 / 3, 1.0])))
 
 
-def test_get_quantile_handles_infinite_values_as_nan():
-    data = np.array([1.0, 2.0, 3.0, np.inf, -np.inf])
-    p = np.array([0.0, 0.5, 1.0])
-
-    quantiles = get_quantile(data, p)
-
-    assert np.allclose(quantiles, np.array([1.0, 2.0, 3.0]))
-
-
-def test_get_prob_uses_only_finite_value_count():
-    data = np.array([1.0, 2.0, 3.0, np.inf, -np.inf])
-    q = np.array([0.5, 2.0, 10.0])
-
-    probs = get_prob(data, q)
-
-    assert np.allclose(probs, np.array([0.0, 2 / 3, 1.0]))
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_utils_multi.py
+++ tests/test_utils_multi.py
@@ -1,4 +1,13 @@
-from dataplot.utils.multi import MultiObject, cleaner, multiple, multipartial, single
+import unittest
+
+from dataplot.utils.multi import (
+    UNSUBSCRIPTABLE,
+    MultiObject,
+    cleaner,
+    multiple,
+    multipartial,
+    single,
+)
 
 
 class Box:
@@ -18,32 +27,59 @@ class F:
         return x + self.offset + (prev or 0)
 
 
-def test_multiobject_getattr_and_call():
-    mo = MultiObject([Box(1), Box(2)])
+class TestMultiUtils(unittest.TestCase):
+    def test_multiobject_getattr_and_call(self):
+        mo = MultiObject([Box(1), Box(2)])
 
-    values = mo.value
-    added = mo.add(3)
+        values = mo.value
+        added = mo.add(3)
 
-    assert values.__multiobjects__ == [1, 2]
-    assert added.__multiobjects__ == [4, 5]
+        self.assertEqual(values.__multiobjects__, [1, 2])
+        self.assertEqual(added.__multiobjects__, [4, 5])
+
+    def test_multiobject_call_with_reflex(self):
+        mo = MultiObject([F(1), F(2)], call_reflex=True)
+
+        result = mo(3)
+
+        self.assertEqual(result.__multiobjects__, [4, 9])
+
+    def test_multipartial_single_multiple_and_cleaner(self):
+        ctor = multipartial(call_reflex=False)
+        mo = ctor([1, 2, 3])
+
+        self.assertIsInstance(mo, MultiObject)
+        self.assertEqual(single(mo, n=1), 2)
+        self.assertEqual(single(99), 99)
+        self.assertEqual(multiple(mo), [1, 2, 3])
+        self.assertEqual(multiple(5), [5])
+        self.assertIsNone(cleaner([None, None]))
+        self.assertEqual(cleaner([None, 1]).__multiobjects__, [None, 1])
+
+    def test_multiobject_binary_operations_for_scalars_and_multiobject(self):
+        mo = MultiObject([1, 2, 3])
+        self.assertEqual((mo + 2).__multiobjects__, [3, 4, 5])
+        self.assertEqual((2 + mo).__multiobjects__, [3, 4, 5])
+
+        mo2 = MultiObject([10, 20, 30])
+        self.assertEqual((mo2 - mo).__multiobjects__, [9, 18, 27])
+
+    def test_multiobject_getitem_behavior(self):
+        seq = MultiObject([[1, 2], [3, 4]])
+        self.assertEqual(seq[0].__multiobjects__, [1, 3])
+
+        class MaybeSubscriptable:
+            def __init__(self, value):
+                self.value = value
+
+            def __getitem__(self, key):
+                if isinstance(key, int):
+                    return UNSUBSCRIPTABLE
+                return self.value[key]
+
+        direct = MultiObject([MaybeSubscriptable("ab"), MaybeSubscriptable("cd")])
+        self.assertEqual(direct[1].value, "cd")
 
 
-def test_multiobject_call_with_reflex():
-    mo = MultiObject([F(1), F(2)], call_reflex=True)
-
-    result = mo(3)
-
-    assert result.__multiobjects__ == [4, 9]
-
-
-def test_multipartial_single_multiple_and_cleaner():
-    ctor = multipartial(call_reflex=False)
-    mo = ctor([1, 2, 3])
-
-    assert isinstance(mo, MultiObject)
-    assert single(mo, n=1) == 2
-    assert single(99) == 99
-    assert multiple(mo) == [1, 2, 3]
-    assert multiple(5) == [5]
-    assert cleaner([None, None]) is None
-    assert cleaner([None, 1]).__multiobjects__ == [None, 1]
+if __name__ == "__main__":
+    unittest.main()
