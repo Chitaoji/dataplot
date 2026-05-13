8b047f345f6aaa9cb7a92e0b0ba741eb56cbcf56
Chitaoji <2360742040@qq.com>
2026-05-13 18:32:19 +0800

    Update

--- src/dataplot/artist/linechart.py
+++ src/dataplot/artist/linechart.py
@@ -39,7 +39,6 @@ class LineChart(Plotter):
     def paint(self, ax: "AxesWrapper", **_) -> None:
         ax.set_axes(
             title=ax.get_setting("title", "Line Chart"),
-            xlabel=ax.get_setting("xlabel", "count"),
             ylabel=ax.get_setting("ylabel", "value"),
         )
         ax.load(self.settings)
