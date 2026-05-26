724ae1ad75deed6a9cf3ae5768cef029b7e103a2
Chitaoji <2360742040@qq.com>
2026-05-26 13:53:37 +0800

    Update

--- src/dataplot/container.py
+++ src/dataplot/container.py
@@ -223,12 +223,7 @@ class AxesWrapper(PlotSettable):
             legend = self.ax.legend(loc=self.get_setting("legend_loc"))
             min_legend_alpha = 0.5
             for handle in legend.legend_handles:
-                handle_alpha = handle.get_alpha()
-                handle.set_alpha(
-                    min_legend_alpha
-                    if handle_alpha is None
-                    else max(handle_alpha, min_legend_alpha)
-                )
+                handle.set_alpha(max(handle.get_alpha(), min_legend_alpha))
         if self.get_setting("grid"):
             alpha = self.get_setting("alpha")
             default_grid_alpha = (
